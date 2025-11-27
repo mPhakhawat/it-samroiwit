94a00cab4bb988fb8cdd2294dcd924a7c6fc9278
mPhakhawat <mpanop.1996@gmail.com>
2025-11-27 15:09:54 +0700

    Delete base.js

--- base.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyA9HV5HiB7W6ITQnKmq4bBxZKecks9cwDc",
-  authDomain: "it-samroiwit.firebaseapp.com",
-  projectId: "it-samroiwit",
-  storageBucket: "it-samroiwit.firebasestorage.app",
-  messagingSenderId: "589045820534",
-  appId: "1:589045820534:web:c4ddb324feb9d121344357",
-  measurementId: "G-9H4TNT71XP"
-};
